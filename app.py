c91341c2cbc11f44b9c5411db078bbfc0b4dc473
neonsk <shota.k.neon@gmail.com>
2019-06-16 00:02:23 +0900

    flask入力フォーム追加

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('first_app.html')
+
+@app.route('/confirm', methods = ['POST', 'GET'])
+def confirm():
+    if request.method == 'POST':
+        result = request.form.getlist("name")
+    return render_template("confirm.html",result = result)
 
 if __name__ == '__main__':
     app.run()
