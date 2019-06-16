8ade325cd14fae223586999cbd6a326aa87200ca
neonsk <shota.k.neon@gmail.com>
2019-06-16 14:19:50 +0900

    confirm.htmlに名前を表示できるように修正

--- app.py
+++ app.py
@@ -2,14 +2,17 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+#最初に表示される画面
 @app.route('/')
 def index():
     return render_template('first_app.html')
 
+#submitされた際に表示される画面
 @app.route('/confirm', methods = ['POST', 'GET'])
 def confirm():
     if request.method == 'POST':
-        result = request.form.getlist("name")
+        result = request.form["Name"]
+    print(result)
     return render_template("confirm.html",result = result)
 
 if __name__ == '__main__':
